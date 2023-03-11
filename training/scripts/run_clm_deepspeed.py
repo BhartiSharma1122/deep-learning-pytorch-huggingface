02ee53e89806316eb43d3eba5cc974b2347bccd8
philschmid <schmidphilipp1995@gmail.com>
2023-03-11 21:59:45 +0000

    added run_clm deepspeed example

---
+++ training/scripts/run_clm_deepspeed.py
@@ -0,0 +1,114 @@
+import os
+import argparse
+import numpy as np
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    set_seed,
+    default_data_collator,
+)
+from datasets import load_from_disk
+import torch
+import numpy as np
+
+from huggingface_hub import HfFolder
+from transformers import Trainer, TrainingArguments
+
+
+def parse_arge():
+    """Parse the arguments."""
+    parser = argparse.ArgumentParser()
+    # add model id and dataset path argument
+    parser.add_argument("--model_id", type=str, default="google/flan-t5-xl", help="Model id to use for training.")
+    parser.add_argument("--dataset_path", type=str, default="data", help="Path to the already processed dataset.")
+    parser.add_argument(
+        "--repository_id", type=str, default=None, help="Hugging Face Repository id for uploading models"
+    )
+    # add training hyperparameters for epochs, batch size, learning rate, and seed
+    parser.add_argument("--epochs", type=int, default=3, help="Number of epochs to train for.")
+    parser.add_argument("--per_device_train_batch_size", type=int, default=8, help="Batch size to use for training.")
+    parser.add_argument("--per_device_eval_batch_size", type=int, default=8, help="Batch size to use for testing.")
+    parser.add_argument("--lr", type=float, default=3e-3, help="Learning rate to use for training.")
+    parser.add_argument("--seed", type=int, default=42, help="Seed to use for training.")
+    parser.add_argument("--deepspeed", type=str, default=None, help="Path to deepspeed config file.")
+    parser.add_argument("--gradient_checkpointing", type=bool, default=True, help="Path to deepspeed config file.")
+    parser.add_argument(
+        "--bf16",
+        type=bool,
+        default=True if torch.cuda.get_device_capability()[0] == 8 else False,
+        help="Whether to use bf16.",
+    )
+    parser.add_argument(
+        "--hf_token",
+        type=str,
+        default=HfFolder.get_token(),
+        help="Token to use for uploading models to Hugging Face Hub.",
+    )
+    args = parser.parse_known_args()
+    return args
+
+
+def training_function(args):
+    # set seed
+    set_seed(args.seed)
+
+    # load dataset from disk and tokenizer
+    train_dataset = load_from_disk(args.dataset_path)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_id)
+    # load model from the hub
+    model = AutoModelForCausalLM.from_pretrained(
+        args.model_id,
+        use_cache=False if args.gradient_checkpointing else True,  # this is needed for gradient checkpointing
+    )
+
+    # Define training args
+    # output_dir = args.repository_id if args.repository_id else args.model_id.split("/")[-1]
+    output_dir = args.model_id.split("/")[-1]
+    training_args = TrainingArguments(
+        output_dir=output_dir,
+        per_device_train_batch_size=args.per_device_train_batch_size,
+        per_device_eval_batch_size=args.per_device_eval_batch_size,
+        bf16=args.bf16,  # Use BF16 if available
+        learning_rate=args.lr,
+        num_train_epochs=args.epochs,
+        deepspeed=args.deepspeed,
+        gradient_checkpointing=args.gradient_checkpointing,
+        # logging & evaluation strategies
+        logging_dir=f"{output_dir}/logs",
+        logging_strategy="steps",
+        logging_steps=200,
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
+        save_total_limit=2,
+        load_best_model_at_end=True,
+        # push to hub parameters
+        report_to="tensorboard",
+        push_to_hub=True if args.repository_id else False,
+        hub_strategy="every_save",
+        hub_model_id=args.repository_id if args.repository_id else None,
+        hub_token=args.hf_token,
+    )
+
+    # Create Trainer instance
+    trainer = Trainer(
+        model=model, args=training_args, train_dataset=train_dataset, data_collator=default_data_collator
+    )
+
+    # Start training
+    trainer.train()
+
+    # Save our tokenizer and create model card
+    tokenizer.save_pretrained(output_dir)
+    trainer.create_model_card()
+    # Push the results to the hub
+    if args.repository_id:
+        trainer.push_to_hub()
+
+
+def main():
+    args, _ = parse_arge()
+    training_function(args)
+
+
+if __name__ == "__main__":
+    main()
